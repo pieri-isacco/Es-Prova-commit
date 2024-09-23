a02b47f3693f13b69594a81d0dd83fc61c0f7c58
lai2-24 <lai2-24@itismeucci.com>
2024-09-23 10:37:09 +0200

    es ripasso thread

--- src/main/java/com/es1/Main.java
+++ src/main/java/com/es1/Main.java
@@ -2,6 +2,13 @@ package com.es1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("cacca!");
+        int n=100;
+
+        MioThread t1= new MioThread(100);
+        MioThread t2= new MioThread(200);
+
+        t1.start();
+        t2.start();
+        
     }
 }
